5584d5ae9b0d1a298a1b3e358583ddb039a2080e
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-03-24 21:18:09 -0300

    add: media responsiveness

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -12,20 +12,20 @@ interface BookCardProps {
 export function BookCard({ title, author, language, snippet, src }: BookCardProps) {
 
   return (
-    <div className="flex w-[21rem] p-3 gap-4 mb-9 bg-[#171c30] rounded-lg">
-      {!src
-        ? <Image src={no_cover} width={100} height={150} alt="book cover" className="rounded-lg h-[9rem]" />
-        : <Image src={src} width={100} height={150} alt="book cover" className="rounded-lg h-[9rem]" />
-      }
+    <div className="flex w-[21rem] lg:w-[24rem] p-3 gap-4 mb-4 bg-[#171c30] rounded-lg">
+      {src
+        ? <Image src={src} width={100} height={150} alt="" title="book cover" className="rounded-lg h-[9rem]" />
+        : <Image src={no_cover} width={100} height={150} alt="" title="book cover" className="rounded-lg h-[9rem]" />}
       <div>
         <h1 className="font-bold text-xl line-clamp-1" title="title">{title}</h1>
         <span className="opacity-70 line-clamp-1" title="author">{author}</span>
         <div className="flex flex-row  text-xs my-1">
-          <span className="outline-1 outline outline-[#8486dd] rounded-md py-1 px-2">{language}</span>
+          <span className="outline-1 outline outline-[#8486dd] rounded-md py-1 px-2" title="language">{language.toUpperCase()}</span>
         </div>
-        {!snippet
-          ? <p className="line-clamp-3 text-sm">This book has no description available.</p>
-          : <p className="line-clamp-3 text-sm">{snippet}</p>}
+        {snippet
+          ? <p className="line-clamp-3 text-sm" title="snippet">{snippet}</p>
+          : <p className="line-clamp-3 text-sm">This book has no description available.</p>
+        }
       </div>
     </div>
   )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { BookCard } from "@/components/BookCard";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { MagnifyingGlass } from "phosphor-react";
 import { useState } from "react";
 
@@ -7,21 +8,23 @@ export default function Home() {
   const [book, setBook] = useState("");
   const [searchResult, setSearchResult] = useState([]);
 
+  const router = useRouter()
+
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const book = event.target.value;
 
     setBook(book)
   }
 
-  console.log(searchResult)
-
   function handleSubmit(event: any) {
     event.preventDefault()
 
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${process.env.NEXT_PUBLIC_KEY}&maxResults=40`)
-      .then(data => {
-        setSearchResult(data.data.items)
-      })
+    if (book) {
+      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${process.env.NEXT_PUBLIC_KEY}&maxResults=40`)
+        .then(data => {
+          setSearchResult(data.data.items)
+        })
+    }
   }
 
   return (
@@ -30,26 +33,25 @@ export default function Home() {
         <input
           type="text"
           onChange={handleChange}
-          placeholder="Search by name"
-          alt="Search by name"
-          className="w-[19rem] xl:w-[32rem] py-[0.875rem] pl-4 placeholder:text-base bg-[#0c0d1b] outline outline-1 outline-[#3c3b8a] rounded-lg focus:outline focus:outline-1 focus:outline-[#1f7eb4]" />
+          placeholder="Search"
+          alt="Search"
+          className="w-[19rem] md:w-[32rem] py-[0.875rem] pl-4 placeholder:text-base bg-[#0c0d1b] outline outline-1 outline-[#3c3b8a] rounded-lg focus:outline focus:outline-1 focus:outline-[#1f7eb4]" />
         <button className=" text-[#bbbfcf] focus:text-[#1f7eb4] hover:text-[#1f7eb4] cursor-pointer">
           <MagnifyingGlass className="text-[1.6rem] ml-[-2.5rem]" alt="Search" type="submit" />
         </button>
       </form>
-
-
-      {searchResult.map(book => (
-        <BookCard
-          key={book.id}
-          src={book.volumeInfo.imageLinks?.thumbnail}
-          title={book.volumeInfo.title}
-          author={book.volumeInfo.authors}
-          language={book.volumeInfo.language}
-          snippet={book.volumeInfo.description}
-        />
-      ))}
-
+      <main className="flex flex-wrap gap-8 justify-center">
+        {searchResult.map(book => (
+          <BookCard
+            key={book.id}
+            src={book.volumeInfo.imageLinks?.thumbnail}
+            title={book.volumeInfo.title}
+            author={book.volumeInfo.authors}
+            language={book.volumeInfo.language}
+            snippet={book.volumeInfo.description}
+          />
+        ))}
+      </main>
     </div>
   )
 }
